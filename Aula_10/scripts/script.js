21af572c0e2faa60ad2e1f892f86ee56c02b4dcd
Allyson Gonçalves <141843515+allysonsv60@users.noreply.github.com>
2023-11-01 09:48:49 -0300

    2

---
+++ Aula_10/scripts/script.js
@@ -0,0 +1,17 @@
+function alterarTitulo() {
+    var novoTexto = prompt("Novo texto para o título:");
+    document.getElementById("titulo").textContent = novoTexto;
+}
+
+function alterarParagrafo1() {
+    var novoTexto = prompt("Novo texto para o primeiro parágrafo:");
+    document.getElementById("paragrafo1").textContent = novoTexto;
+}
+
+function alterarTitulo2() {
+    var novoTexto = prompt("Novo texto para o titulo:");
+    document.getElementById("titulo2").textContent = novoTexto;
+}
+
+//==========================================================================
+//==========================================================================
