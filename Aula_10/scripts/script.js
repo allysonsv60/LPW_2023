3fd4f2860142fb2a6761112807a0b3f2fe407443
Allyson Gonçalves <141843515+allysonsv60@users.noreply.github.com>
2023-11-01 10:57:44 -0300

    8

--- Aula_10/scripts/script.js
+++ Aula_10/scripts/script.js
@@ -18,14 +18,14 @@ function alterarTitulo2() {
 
 function alterarP1() {
     var p1 = prompt [0];
-    for (var i = 1; i <= 4; i++) {
+   
        
         var p1 = prompt("Digite o novo p:");
         document.getElementsByTagName("p")[0].innerHTML = p1;
         document.getElementsByTagName("p")[1].innerHTML = p1;
         document.getElementsByTagName("p")[2].innerHTML = p1;
         document.getElementsByTagName("p")[3].innerHTML = p1;
-    }
+    
 }
   
    
